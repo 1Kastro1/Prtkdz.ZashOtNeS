2524c9135fabc14e2ed4733b40589b9c5500990e
камиль исмаилов <sgfejs99@gmail.com>
2020-05-21 15:30:34 +0300

    Добавьте файлы проекта.

---
+++ Prtkdz.ZashOtNeS/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prtkdz.ZashOtNeS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "admin")
+            {
+                Form2 f2;
+                f2 = new Form2();
+                f2.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели неверный пароль. Попробуйте ещё раз");
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                button1_Click(sender, e);
+            }
+        }
+    }
+}
